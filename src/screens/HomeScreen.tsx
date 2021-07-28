03d8745c5325fa47942742ded3724c833bf85b00
josecbm <jcbautista95@gmail.com>
2021-07-28 10:31:11 -0600

    189

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,14 +1,71 @@
 import React from 'react'
 import { Text, View } from 'react-native';
+import { FlatList } from 'react-native-gesture-handler';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
 import Icon from 'react-native-vector-icons/Ionicons';
+import { styles } from '../theme/appTheme';
+
+interface MenuItem{
+    name: string;
+    icon: string;
+    components: string;
+}
+
+const menuItems = [
+    {
+    name: 'Animation 101',
+    icon: 'cube-outline',
+    components: 'Animation101Screen'
+    },
+    {
+        name: 'Animation 102',
+        icon: 'albums-outline',
+        components: 'Animation102Screen'
+    }
+]
 export const HomeScreen = () => {
+
+    const {top } = useSafeAreaInsets();
+
+    const renderMenuItem = (menuItem: MenuItem) =>{
+        return (
+            <View>
+                <Text> {menuItem.name} - {menuItem.icon}</Text>
+            </View>
+        )
+    }
+
+    const renderListHeader = () => {
+        return (
+            <View style={{ marginTop: top + 20, marginBottom: 20}}>
+                <Text style={styles.title}>Opciones de menu</Text>
+            </View>
+        )
+    }
+
+    const itemSeparator = () => {
+        return (
+            <View
+                style ={{
+                    borderBottomWidth: 1,
+                    opacity: 0.4,
+                    marginVertical: 8
+                }}
+            >
+
+            </View>
+        );
+    }
     return (
-        <View>
-            <Icon
-                name="star-outline"
-                size={50}
-                color="black"
+        <View style={{flex:1, ... styles.globalMargin}}>
+            
+            <FlatList
+                data={menuItems}
+                renderItem={({item}) => renderMenuItem( item )}
+                keyExtractor = {(item) => item.name}
+                ListHeaderComponent = {()=>renderListHeader()}
+                ItemSeparatorComponent = {itemSeparator}
             />
         </View>
     )
